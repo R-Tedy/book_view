903565273f3b15eb213f025de3e48251eab7a88b
R-Tedy <stedyroland2@gmail.com>
2025-10-14 08:31:44 +0300

    impliment registration route

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port:`, PORT);
