4b20b9077a9cf2a8420e81c9470602748a0f3254
R-Tedy <stedyroland2@gmail.com>
2025-10-16 05:59:27 +0300

    compelete book routes

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -71,6 +71,29 @@ router.get("/user", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({message: "Book not found"});
 
+    if (book.user.toString() !== req.user._id.toString()) return res.status(401).json({message: "Unauthorized"});
+
+    if (book.image && book.image.includes("cloudinary")) {
+      try {
+        const publicId = book.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (error) {
+        console.error("Error deleting image from cloudinary:", error);
+      };
+    };
+
+    await book.deleteOne();
+
+    res.json({message: "Book deleted successfully"})
+  } catch (error) {
+    console.error("Error deleting book:", error);
+    res.status(500).json({message: "Internal server error"});
+  }
+})
 
 export default router;
