64fb6de1463ebb88b404576c74d6ffff46857cf1
R-Tedy <stedyroland2@gmail.com>
2025-10-16 03:11:59 +0300

    impliment cloudinary config

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port:`, PORT);
